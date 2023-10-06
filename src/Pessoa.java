6338eea62c2d05bf464da0df78e797d1f700ba0e
Caue <starbestbr@gmail.com>
2023-10-06 10:11:52 -0300

    Atividade completa avançada

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,10 +2,21 @@ public class Pessoa {
 
     private String nome;
     private int idade;
+    private String genero;
+    private String estadoCivil;
+    private String hobby;
+    private Boolean estuda;
+    private Boolean trabalha;
 
-    public Pessoa(){
-        nome = "Cauê";
-        idade = 17;
+
+    public Pessoa(String nome, int idade, String genero, String estadoCivil, String hobby, Boolean estuda, Boolean trabalha) {
+        this.nome = nome;
+        this.idade = idade;
+        this.genero = genero;
+        this.estadoCivil = estadoCivil;
+        this.hobby = hobby;
+        this.estuda = estuda;
+        this.trabalha = trabalha;
     }
 
     public String getNome(){
@@ -16,6 +27,12 @@ public class Pessoa {
         return idade;
     }
 
+    public String getGenero(){ return genero; }
+    public String getEstadoCivil(){ return estadoCivil; }
+    public String getHobby(){ return hobby; }
+    public Boolean getEstuda(){ return estuda; }
+    public Boolean getTrabalha(){ return trabalha; }
+
     public void setNome(String newNome){
         this.nome = newNome;
     }
@@ -23,5 +40,10 @@ public class Pessoa {
     public void setIdade(int newIdade){
         this.idade = newIdade;
     }
+    public void setGenero(String newGenero){ this.genero = newGenero;}
+    public void setEstadoCivil(String newEstadoCivil){ this.estadoCivil = newEstadoCivil;}
+    public void setHobby (String newHobby){ this.hobby = newHobby;}
+    public void setEstuda (Boolean newEstuda){ this.estuda = newEstuda;}
+    public void setTrabalha(Boolean newTrabalha){ this.trabalha = newTrabalha;}
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,12 +1,24 @@
 public class Principal {
     public static void main(String[]args){
-        Pessoa minhaPessoa = new Pessoa();
+        Pessoa minhaPessoa = new Pessoa("Caue", 17, "Homem", "solteiro", "jogos", true, true);
         minhaPessoa.getNome();
         minhaPessoa.getIdade();
-        System.out.println(minhaPessoa.getNome() + " tem " + minhaPessoa.getIdade() + " anos.");
+        minhaPessoa.getGenero();
+        minhaPessoa.getEstadoCivil();
+        minhaPessoa.getHobby();
+        minhaPessoa.getEstuda();
+        minhaPessoa.getTrabalha();
+        System.out.println(minhaPessoa.getNome() + " tem " + minhaPessoa.getIdade() + " anos, é " + minhaPessoa.getGenero() + ", está " +  minhaPessoa.getEstadoCivil() + ", gosta de " +
+        minhaPessoa.getHobby() +  ". Respectivamente, estuda e trabalha? " + minhaPessoa.getEstuda() + " " +  minhaPessoa.getTrabalha() );
         minhaPessoa.setNome("Nana");
         minhaPessoa.setIdade(20);
-        System.out.println(minhaPessoa.getNome() + " tem " + minhaPessoa.getIdade() + " anos.");
+        minhaPessoa.setGenero("Mulher");
+        minhaPessoa.setEstadoCivil("Solteira");
+        minhaPessoa.setHobby("academia");
+        minhaPessoa.setEstuda(false);
+        minhaPessoa.setTrabalha(false);
+        System.out.println(minhaPessoa.getNome() + " tem " + minhaPessoa.getIdade() + " anos, é " + minhaPessoa.getGenero() + ", está " +  minhaPessoa.getEstadoCivil() + ", gosta de " +
+                minhaPessoa.getHobby() +  ". Respectivamente, estuda e trabalha? " + minhaPessoa.getEstuda() + " " +  minhaPessoa.getTrabalha() );
 
     }
 }
